c20e9593551212ebcedbc30844a63c4552e35ce6
Nicholas Odinaka <nicholas.okeke87@gmail.com>
2023-11-16 00:06:59 +0100

    repo setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="text-2xl">
+          Welcome to Insure ... Please wait
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

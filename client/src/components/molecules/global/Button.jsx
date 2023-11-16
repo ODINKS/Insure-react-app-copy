df63a56a1bf06eff46e08d9459cfe53c2e81da2d
Nicholas Odinaka <nicholas.okeke87@gmail.com>
2023-11-16 15:08:20 +0100

    created button component

---
+++ client/src/components/molecules/global/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = (props) => {
+    const {description, width} = props
+
+  return (
+    <div>
+      <button className={`bg-[#e76927] text-white font-bold px-6 py-3 rounded-[8px] text-bold text-sm m-2 hover:bg-[#ffa074] hover:text-[#ffffff] w-${width}px`}>
+        <a href="">{description}</a>
+      </button>
+    </div>
+  )
+}
+
+export default Button

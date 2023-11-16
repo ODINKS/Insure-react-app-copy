113ca5439db8862e59135f59255c722e101aabc4
Mary Samuel <samuelmary1617@gmail.com>
2023-11-16 03:14:37 -0800

    shorten url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div >
       <Home/>
+      {/* <Contact /> */}
     </div>
   );
 }

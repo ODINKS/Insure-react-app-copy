3e45c8901ce245ff6527f4175e8d06e88827c66f
Ultra-tech-code <hizick27@gmail.com>
2023-11-17 03:32:53 +0100

    abous us page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,31 @@ import './App.css';
 // import Home from './pages/Home';
 // import Contact from './pages/Contact';
 // import HeroSection from './components/molecules/global/HeroSection'
-import Button from './components/molecules/global/Butt
+import Button from './components/molecules/global/Button';
+import About from './pages/About';
 
-import Home from "./pages/Home";
 
-function App() {
-  const buttonDesc = `Get started`
-  const buttonWidth = `500`
+import Home from "./pages/Home"
+
+
+
+import React from 'react'
+
+const App = () => {
+  const buttonDesc = `Get started`;
+  const buttonWidth = `500`;
+
   return (
-    <div >
+    <div>
       {/* <Home/> */}
       {/* <Contact /> */}
       {/* <HeroSection title={title} img={img} description={description} /> */}
-      <Button description={buttonDesc} width={buttonWidth} />
+      {/* <Button description={buttonDesc} width={buttonWidth} /> */}
+      <About />
 
-    <div>
-  );
+
+    </div>
+  )
 }
 
-export default App;
+export default App

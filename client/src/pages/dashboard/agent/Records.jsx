d672103097b9a86c2417d3052865128a313fd26c
Mary Samuel <samuelmary1617@gmail.com>
2023-11-21 02:32:07 -0800

    followed insure styling rules

--- client/src/pages/dashboard/agent/Records.jsx
+++ client/src/pages/dashboard/agent/Records.jsx
@@ -7,12 +7,12 @@ function AgentRecord() {
   };
 
   const buttonStyle = 'bg-orange-500 text-white py-2 px-9 rounded mr-4 relative';
-  const tableStyle = 'bg-white text-black border-collapse w-[100%] lg:w-[60%] mt-9 rounded-lg overflow-hidden relative';
-  const tableHeadingStyle = 'border-b-2 p-2 font-bold border-b-2 border-gray-500 ';
-  const tableRowStyle = 'p-2 border-t-2 border-gray-500';
+  const tableStyle = 'bg-white text-black text-sm border-collapse w-[100%] lg:w-[60%] mt-9 rounded-lg overflow-hidden relative';
+  const tableHeadingStyle = 'border-b-2 p-2 font-semi-bold text-lg border-b-2 border-gray-500 ';
+  const tableRowStyle = ' p-2 border-t-2 border-gray-500';
 
   const paginationContainerStyle = 'absolute bottom-0 right-0 mb-[-5px]';
-  const paginationStyle = 'cursor-pointer text-black px-2 py-1 hover:bg-orange-500 hover:text-white';
+  const paginationStyle = 'cursor-pointer text-black text-sm px-2 py-1 hover:bg-orange-500 hover:text-white';
 
 
   const [isDropdownOpen, setDropdownOpen] = useState(false);

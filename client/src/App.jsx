3611ea16db8394840c9ee9a276a79d64634bbf31
Nicholas Odinaka <nicholas.okeke87@gmail.com>
2023-11-22 02:15:39 +0100

    Fixed the footer of the landing pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import About from './pages/About';
 import Login from './pages/auth/admin/login'
 import Reset from './pages/auth/admin/reset'
 import TEAMINVITE from './pages/auth/admin/teamInvite'
+import AdminRecord from './pages/dashboard/admin/Records';
+import AgentRecord from './pages/dashboard/agent/Records';
 // import Navbar from './components/molecules/global/Navbar';
 // import Contact from './pages/Contact';
 // import Button from './components/molecules/global/Button';
@@ -18,21 +20,22 @@ import { Registration } from './pages/auth/admin/Registration';
 
 function App() {
   return (
-      // <div>
-      //     {/* <Navbar /> */}
-      // <Home />
-      //     {/* <div className='w-full h-[90vh] flex justify-center items-center'>
-      //         <h1 className='text-4xl text-[--black-text]'>Welcome</h1>
-      //     </div> */}
-      // </div>
+      <div>
+          {/* <Navbar /> */}
+          {/* <div className='w-full h-[90vh] flex justify-center items-center'>
+              <h1 className='text-4xl text-[--black-text]'>Welcome</h1>
+          </div> */}
+          {/* <AdminRecord /> */}
+          {/* <AgentRecord /> */}
+      </div>
     
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/admin-signup-one" element={<Registration />} />
-              <Route path="/admin-login" element={<Login />} />
-          </Routes>
+          // <Routes>
+          //     <Route path="/" element={<Home />} />
+          //     <Route path="/about" element={<About />} />
+          //     <Route path="/contact" element={<Contact />} />
+          //     <Route path="/admin-signup-one" element={<Registration />} />
+          //     <Route path="/admin-login" element={<Login />} />
+          // </Routes>
   );
 }
 

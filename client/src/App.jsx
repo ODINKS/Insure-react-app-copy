2e30daa06048fac9a800aa0cfb37027c3b404013
Ultra-tech-code <hizick27@gmail.com>
2023-11-23 22:07:34 +0100

    resolved conflic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,8 @@ import AdminHome from './pages/dashboard/admin/AdminHome';
 import AgentHome from './pages/dashboard/agent/AgentHome';
 import Reset from './pages/auth/admin/reset'
 import TEAMINVITE from './pages/auth/admin/teamInvite'
-<<<<<<< HEAD
- import Navbar from './components/molecules/global/Navbar';
-  import Button from './components/molecules/global/Button';
-=======
 import Navbar from './components/molecules/global/Navbar';
 import Button from './components/molecules/global/Button';
->>>>>>> ce24f7db88c6a41c1e2b5cb374f31df71457cde1
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Registration } from './pages/auth/admin/Registration';
 import AdminSidebar from './components/molecules/dashboard/AdminSidebar';
@@ -24,8 +19,6 @@ import Pricing from './pages/Pricing';
 // import { RegAccount } from './pages/auth/admin/RegAccount';
 // import AdminRecord from './pages/dashboard/admin/Records';
 // import AgentRecord from './pages/dashboard/agent/Records';
-
-
 
 
 
@@ -49,16 +42,11 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/admin-signup-one" element={<Registration />} />
-<<<<<<< HEAD
               <Route path="/pricing" element={<Pricing />} />
               <Route path="/admin-login" element={<Login />} />
               <Route path="/dashboard/admin/Home" element={<AdminHome />} />
               <Route path="/dashboard/agent/Home" element={<AgentHome />} />
-=======
-              <Route path="/pricing" element={<Pricing />} /> */}
-              {/* //<Route path="/dashboard/admin/Home" element={<AdminHome />} /> */}
-              <Route path="/" element={<Button />} />
->>>>>>> ce24f7db88c6a41c1e2b5cb374f31df71457cde1
+               <Route path="/" element={<Button />} /> */}
           </Routes>
     );
 }

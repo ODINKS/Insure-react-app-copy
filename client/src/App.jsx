0e10f15d60ecc2c4cb55f3e97cc464f49cedec58
Mary Samuel <samuelmary1617@gmail.com>
2023-11-23 01:40:48 -0800

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,15 +6,15 @@ import Login from './pages/auth/admin/login'
 import AdminHome from './pages/dashboard/admin/Home';
 import Reset from './pages/auth/admin/reset'
 import TEAMINVITE from './pages/auth/admin/teamInvite'
-// import Navbar from './components/molecules/global/Navbar';
-// import Contact from './pages/Contact';
-// import Button from './components/molecules/global/Button';
+ import Navbar from './components/molecules/global/Navbar';
+ import Contact from './pages/Contact';
+ import Button from './components/molecules/global/Button';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Registration } from './pages/auth/admin/Registration';
 import AdminSidebar from './components/molecules/dashboard/AdminSidebar';
 import AgentSidebar from './components/molecules/dashboard/AgentSidebar';
-// import { Reg } from './pages/auth/admin/Reg';
-// import { RegAccount } from './pages/auth/admin/RegAccount';
+ import { Reg } from './pages/auth/admin/Reg';
+ import { RegAccount } from './pages/auth/admin/RegAccount';
 import AdminRecord from './pages/dashboard/admin/Records';
 import AgentRecord from './pages/dashboard/agent/Records';
 
@@ -35,14 +35,14 @@ function App() {
           <AdminRecord />
       </div>
     
-          // <Routes>
-          //     <Route path="/" element={<Home />} />
-          //     <Route path="/about" element={<About />} />
-          //     <Route path="/contact" element={<Contact />} />
-          //     <Route path="/admin-signup-one" element={<Registration />} />
-          //     <Route path="/admin-login" element={<Login />} />
-          //     <Route path="/dashboard/admin/Home" element={<AdminHome />} />
-          // </Routes>
+        //   <Routes>
+        //       <Route path="/" element={<Home />} />
+        //       <Route path="/about" element={<About />} />
+        //       <Route path="/contact" element={<Contact />} />
+        //       <Route path="/admin-signup-one" element={<Registration />} />
+        //       <Route path="/admin-login" element={<Login />} />
+        //       <Route path="/dashboard/admin/Home" element={<AdminHome />} />
+        //   </Routes>
   );
 }
 

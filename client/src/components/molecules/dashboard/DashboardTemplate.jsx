c3ecff0bd32c48ff03d319f0b4cf842a78446401
Nicholas Odinaka <nicholas.okeke87@gmail.com>
2023-11-28 04:49:03 +0100

    Added an erro page

--- client/src/components/molecules/dashboard/DashboardTemplate.jsx
+++ client/src/components/molecules/dashboard/DashboardTemplate.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route, Switch, Link, useRoute} from 'react-router-dom'
 
 const DashboardTemplate = (props) => {
     const {sidebar, dashboardNav, dashboardBody, notificationSection} = props

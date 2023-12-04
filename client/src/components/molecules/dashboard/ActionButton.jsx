a795a279a780da25e2a25e10d20ee124fa184202
Nicholas Odinaka <nicholas.okeke87@gmail.com>
2023-12-04 21:23:47 +0100

    Adjusted the records page

--- client/src/components/molecules/dashboard/ActionButton.jsx
+++ client/src/components/molecules/dashboard/ActionButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ActionButton = (props) => {
   return (
-    <div className='bg-[--orange-bg] text-white py-2 px-9 rounded mr-4 relative'>{props.title}</div>
+    <button className='bg-[--orange-bg] text-white py-2 px-9 rounded mr-4 relative' onClick={props.ActionButton}>{props.title}</button>
   )
 }
 

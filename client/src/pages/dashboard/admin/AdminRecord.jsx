917a526c2620e3f38dd8de23b00d9381873d15ca
Oluwatosin <hezealice@gmail.com>
2023-12-14 13:31:05 +0100

    fixed record on admin

--- client/src/pages/dashboard/admin/AdminRecord.jsx
+++ client/src/pages/dashboard/admin/AdminRecord.jsx
@@ -71,17 +71,11 @@ const AdminRecords = () => {
           <ActionButton title='EXCEL' onClick={handleExcelClick} />
           <ActionButton title='PRINT' onClick={handlePrintClick} /> */}
 
-
-
-        {/* <ActionButton title='PDF' 
+          {/* <ActionButton title='PDF' 
         action={generatePDF('table-container', 'document')} 
         />
           <ActionButton title='EXCEL' onClick={() => generateExcel('table-container', 'document')} />
           <ActionButton title='PRINT' onClick={() => printContent('table-container')} /> */}
-          <ActionButton title = 'Add New' onClick={handleButtonClick} />
-          {isOverlayVisible && <FormOverlay onClose={handleOverlayClose} />}
-
-
         </div>
       </div>
 

8d8d576902d65fb12b7df07f87c942623825e9fe
OgbajeLeo <leonardaromeh@gmail.com>
2023-12-19 14:48:43 +0100

    newUpdate

--- client/src/pages/auth/admin/AdminLogin.jsx
+++ client/src/pages/auth/admin/AdminLogin.jsx
@@ -12,6 +12,7 @@ const AdminLogin = () => {
     password: "",
   });
   const [isLoading, setIsLoading] =useState(false)
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError]=useState({})
   const navigate = useNavigate();
   const { login } = useAuth();
@@ -40,7 +41,9 @@ const AdminLogin = () => {
   };
 
 
-
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
 
 
   const handleSubmit = async (e) => {
@@ -109,23 +112,40 @@ const AdminLogin = () => {
         <h1 className="font-bold mb-4 text-2xl lg:text-4xl">
             Welcome <span className="text-[--orange-bg]">Back</span> Admin
           </h1>
-        <div className="flex flex-col w-full my-2">
+        {/* <div className="flex flex-col w-full my-2"> */}
           <input
             type="email"
             value={formData.email}  
             onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-            className="px-2 py-3 my-2 rounded-[3px] border border-blue-500 focus:outline-none"
+            className="px-2 w-full h-[40px] py-3 my-2 rounded-[3px] border border-blue-500 focus:outline-none"
             placeholder="Enter email address"
             required
           />
           {error.email && <p className="text-red-500 text-[12px]">{error.email}</p>}
+
+          <div className="relative">
           <input
-            type="password"
+            type={showPassword ? 'text' : 'password'}
             value={formData.password}
             onChange={(e) =>setFormData({ ...formData, password: e.target.value })}
-            className={`px-2 py-3 my-2 rounded-[3px] border border-blue-500 focus:outline-none" ${error.email ? 'border-red-500' : ''}`}
+            className={`w-full h-[40px] px-2 py-3 my-2 rounded-[3px] border border-blue-500 focus:outline-none" ${error.email ? 'border-red-500' : ''}`}
             placeholder="Enter password"
           />
+          <span
+              className="absolute right-4 top-4 cursor-pointer"
+              onClick={handleTogglePassword}
+            >
+              {showPassword ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+              )}
+            </span>
           {error.password && <p className="text-red-500 text-[12px]">{error.password}</p>}
         </div>
        
